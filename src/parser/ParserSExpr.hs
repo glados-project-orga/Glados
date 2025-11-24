e0f482547f7d9c6d69ee38af7a323f5b590b9e0b
celianrag <celian.raguin@epitech.eu>
2025-11-25 00:50:53 +0100

    feat(sexprParser): adding parser of string that gets SExpritems

---
+++ src/parser/ParserSExpr.hs
@@ -0,0 +1,35 @@
+module ParserSExpr (
+    parseSExpr,
+) where
+
+import Ast.LispAst(SExpr(..), getSymbol, getList, getInterger)
+import Parser
+import Data.Char (isSpace)
+import Control.Applicative
+
+parseSymboleChar :: Parser Char
+parseSymboleChar = Parser f
+  where
+    f [] = Left "Reached end of input"
+    f (x:xs)
+      | not (isSpace x) && x /= '(' && x /= ')' = Right (x, xs)
+      | otherwise = Left $ "Invalid token char: " ++ show x
+
+parseSSymbol :: Parser SExpr
+parseSSymbol = SSymbol <$> some parseSymboleChar
+
+parseSNumber :: Parser SExpr
+parseSNumber = SInt <$> parseInt
+
+parseSList :: Parser SExpr
+parseSList = SList <$> (parseChar '(' *> many parseSExpr <* parseChar ')')
+
+parseSExpr :: Parser SExpr
+parseSExpr = betweenSpaces (
+    parseSNumber <|>
+    parseSSymbol <|>
+    parseSList
+    )
+
+-- main :: IO ()
+-- main = print (runParser parseSExpr "(hello (42 world) 123)")
