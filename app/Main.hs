448160b2ff8694dbea14c0e6b80b434cc8382f75
celianrag <celian.raguin@epitech.eu>
2025-11-25 00:50:07 +0100

    feat(main): adding main with basic example use of sexpr parser

--- app/Main.hs
+++ app/Main.hs
@@ -7,8 +7,17 @@
 
 module Main (main) where
 
-import Lib
+import Ast.LispAst
+import ParserSExpr
 import Parser
 
 main :: IO ()
-main = someFunc
+main =
+    case runParser parseSExpr string of
+      Left err -> putStrLn ("Parse error: " ++ err)
+      Right (sexpr, _) ->
+        case sexprToAST sexpr of
+            Nothing -> putStrLn "Could not convert SExpr to AST"
+            Just ast -> print ast
+    where
+      string = "(1 \"hello\" 1)"
