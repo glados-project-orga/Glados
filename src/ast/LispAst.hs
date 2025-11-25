0db001942c2f0f038d9881eb4d3010fee9c781fb
Kerwanc <kerwan.c@gmail.com>
2025-11-25 17:27:44 +0100

    feat(ast): removed useless functions from bootsrap

--- src/ast/LispAst.hs
+++ src/ast/LispAst.hs
@@ -10,30 +10,6 @@ import Data.Maybe()
 import Builtins (apply)
 import Data(Ast(..), SExpr(..))
 
-getSymbol :: SExpr -> Maybe String
-getSymbol (SSymbol mp) = Just mp
-getSymbol _            = Nothing
-
-getInterger :: SExpr -> Maybe Int
-getInterger (SInt mp) = Just mp
-getInterger _         = Nothing
-
-getList :: SExpr -> Maybe [SExpr]
-getList (SList mp) = Just mp
-getList  _         = Nothing
-
-pretty :: SExpr -> String
-pretty (SSymbol stt) = "a Symbol '" ++ stt ++ "'"
-pretty (SInt ii)     = "a Number " ++ show ii
-pretty (SList [])    = "an empty List"
-pretty (SList (x:xs)) =
-  "a List with " ++ pretty x ++
-  concatMap (\e -> " followed by " ++ pretty e) xs
-
-printTree :: SExpr -> Maybe String
-printTree expr = Just (pretty expr)
-
-
 sexprToAST :: SExpr -> Maybe Ast
 sexprToAST (SInt mp) = Just (AInt mp)
 sexprToAST (SSymbol ll) = Just (ASymbol ll)
