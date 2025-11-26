ee2800015db34ed856bbcb95d1bc9c0d0c1cc7b1
4urel PLIY4 <aurel.pliya@epitech.eu>
2025-11-26 11:37:23 +0100

    feat : modify the sexprToAST function for transform espr to ast, fix the Ast and start the evel

--- src/ast/Builtins.hs
+++ src/ast/Builtins.hs
@@ -10,9 +10,35 @@ module Builtins (
 ) where
 
 import Data(Ast(..))
+import qualified Data.Map as Map
 
-apply :: String -> [Ast] -> Maybe Ast
-apply "+" [AInt a, AInt b] = Just (AInt (a + b))
-apply "-" [AInt a, AInt b] = Just (AInt (a - b))
-apply "*" [AInt a, AInt b] = Just (AInt (a * b))
-apply _ _ = Nothing
+-- apply :: String -> [Ast] -> Maybe Ast
+-- apply "+" [AInt a, AInt b] = Just (AInt (a + b))
+-- apply "-" [AInt a, AInt b] = Just (AInt (a - b))
+-- apply "*" [AInt a, AInt b] = Just (AInt (a * b))
+-- apply _ _ = Nothing
+
+apply :: Map.Map String ([Ast] -> Either String Ast)
+apply = Map.fromList
+  [ ("+", builtinAdd),
+    ("*", builtinMul),
+    ("-", builtinSub),
+    ("mod", builtinMod),
+    ("div", builtinDiv)
+  ]
+
+builtinAdd :: [Ast] -> Either String Ast
+builtinAdd _ = Left "Error in operation"
+
+
+builtinMul :: [Ast] -> Either String Ast
+builtinMul _ = Left "Error in operation"
+
+builtinSub :: [Ast] -> Either String Ast
+builtinSub _ = Left "Error in operation"
+
+builtinMod :: [Ast] -> Either String Ast
+builtinMod _ = Left "Error in operation"
+
+builtinDiv :: [Ast] -> Either String Ast
+builtinDiv _ = Left "Error in operation"
--- src/ast/Eval.hs
+++ src/ast/Eval.hs
@@ -10,14 +10,35 @@ module Eval (
 ) where
 
 import Data(Ast(..))
-import Builtins (apply)
+-- import Builtins (apply)
 
-evalAST :: Ast -> Maybe Ast
-evalAST (AInt li) = Just (AInt li)
-evalAST (ASymbol _) = Nothing
-evalAST (ACall (ASymbol fn) args) =
-  case traverse evalAST args of
-    Just vals -> apply fn vals
-    Nothing   -> Nothing
-evalAST (AList mp) = Just (AList mp)
-evalAST _ = Nothing
+import qualified Data.Map as Map
+type Env = Map.Map String Ast
+
+-- evalAST :: Ast -> Maybe Ast
+-- evalAST (AInt li) = Just (AInt li)
+-- evalAST (ASymbol _) = Nothing
+-- evalAST (ACall (ASymbol fn) args) =
+--   case traverse evalAST args of
+--     Just vals -> apply fn vals
+--     Nothing   -> Nothing
+-- evalAST (AList mp) = Just (AList mp)
+-- evalAST _ = Nothing
+
+evalAST :: Env -> Ast -> Either String (Env, Ast)
+evalAST env (AInt li) = Right (env, AInt li)
+
+evalAST env (ASymbol symb) =
+  case Map.lookup symb env of
+    Just val -> Right (env, val)
+    Nothing -> Left ("Unbound symbol: " ++ symb)
+
+evalAST env (ADefine name expr) =
+  case evalAST env expr of
+    Right(_, val) -> Right(Map.insert name val env, val)
+    Left err -> Left err
+
+-- evalAST env (ACall (ASymbol name) args) =
+
+
+evalAST _ baned = Left ("No correct Ast formar" ++ show baned)
