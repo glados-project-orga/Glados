bb678caabb438e0a72a767be62c0c0d8386df9c8
Kerwanc <kerwan.c@gmail.com>
2025-11-26 02:00:48 +0100

    style(test): added a header and reformat indentation to suit the norme

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,14 @@
+{- 
+-- EPITECH PROJECT, 2025
+-- epi-repo
+-- File description:
+-- Spec.hs
+-}
 
-module Main (main) where
+module Main (
+    main
+) where
+    
 import Test.HUnit
 
 tests :: Test
