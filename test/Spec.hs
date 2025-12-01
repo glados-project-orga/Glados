b6ccc0fb248d84ff597d082f4e36b1d3d061b970
Kerwanc <kerwan.c@gmail.com>
2025-12-01 17:57:53 +0100

    feat(lisp-builtin): added new unit test for div and mod function

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,8 +10,14 @@ module Main (
 ) where
 
 import Test.HUnit
-import Builtins(builtinAdd, builtinSub, builtinMul, builtinMod, builtinDiv)
+import Builtins(foundInt, builtinAdd, builtinSub, builtinMul, builtinMod, builtinDiv)
 import Data(Ast(..))
+
+testFoundInt :: Test
+testFoundInt = TestList[
+    TestCase(assertEqual "foundInt AInt 5" (Right 5) (foundInt (AInt 5))),
+    TestCase(assertEqual "foundInt ASymbol foo" (Left "foo is not a number") (foundInt (ASymbol "foo")))
+    ]
 
 testBuiltinsAdd :: Test
 testBuiltinsAdd = TestList[
@@ -22,29 +28,33 @@ testBuiltinsAdd = TestList[
 
 testBuiltinsSub :: Test
 testBuiltinsSub = TestList[
+    TestCase(assertEqual "(-)" (Left "Exception: incorrect argument count in call (-)") (builtinSub [])),
     TestCase(assertEqual "(- 2 1)" (Right (AInt 1)) (builtinSub [AInt 2, AInt 1])),
     TestCase(assertEqual "(- 6 2 1)" (Right (AInt 3)) (builtinSub [AInt 6, AInt 2, AInt 1]))
     ]
 
 testBuiltinsMul :: Test
 testBuiltinsMul = TestList[
+    TestCase(assertEqual "(*)" (Right (AInt 1)) (builtinMul [])),
     TestCase(assertEqual "(* 2 2)" (Right (AInt 4)) (builtinMul [AInt 2, AInt 2])),
     TestCase(assertEqual "(* 2 2 3)" (Right (AInt 12)) (builtinMul [AInt 2, AInt 2, AInt 3]))
     ]
 
 testBuiltinsDiv :: Test
 testBuiltinsDiv = TestList[
-    TestCase(assertEqual "(div 2 2)" (Right (AInt 1)) (builtinDiv [AInt 2, AInt 2])),
-    TestCase(assertEqual "(div 8 2 2)" (Left "expected exactly 2 integers") (builtinDiv [AInt 2, AInt 2, AInt 3]))
+    TestCase(assertEqual "(div 1)" (Left "Exception: incorrect argument count in call (div 1)") (builtinDiv [AInt 1])),
+    TestCase(assertEqual "(div 2 2)" (Right (AInt 1)) (builtinDiv [AInt 2, AInt 2]))
     ]
 
 testBuiltinsMod :: Test
 testBuiltinsMod = TestList[
+    TestCase(assertEqual "(mod 1)" (Left "Exception: incorrect argument count in call (mod 1)") (builtinMod [AInt 1])),
     TestCase(assertEqual "(mod 5 2)" (Right (AInt 1)) (builtinMod [AInt 5, AInt 2]))
     ]
 
 testBuiltins :: Test
 testBuiltins = TestList[
+    testFoundInt,
     testBuiltinsAdd,
     testBuiltinsSub,
     testBuiltinsMul,
