242fa887123126a3a4c1046f3def79c581d03531
Kerwanc <kerwan.c@gmail.com>
2025-12-01 10:24:50 +0100

    feat(lisp-builtin): added unit test for mod and div function

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,33 +10,51 @@ module Main (
 ) where
 
 import Test.HUnit
-import Builtins(apply)
+import Builtins(builtinAdd, builtinSub, builtinMul, builtinMod, builtinDiv)
 import Data(Ast(..))
 
 testBuiltinsAdd :: Test
 testBuiltinsAdd = TestList[
-    TestCase(assertEqual "2 + 2" (Just (AInt 4)) (apply "+" [AInt 2, AInt 2])),
-    TestCase(assertEqual "1 + 2 + 3" (Just (AInt 6)) (apply "+" [AInt 1, AInt 2, AInt 3]))
+    TestCase(assertEqual "(+)" (Right (AInt 0)) (builtinAdd [])),
+    TestCase(assertEqual "(+ 2 2)" (Right (AInt 4)) (builtinAdd [AInt 2, AInt 2])),
+    TestCase(assertEqual "(+ 1 2 3)" (Right (AInt 6)) (builtinAdd [AInt 1, AInt 2, AInt 3]))
     ]
 
 testBuiltinsSub :: Test
 testBuiltinsSub = TestList[
-    TestCase(assertEqual "2 - 1" (Just (AInt 1)) (apply "-" [AInt 2, AInt 1])),
-    TestCase(assertEqual "6 - 2 - 1" (Just (AInt 3)) (apply "-" [AInt 6, AInt 2, AInt 1]))
+    TestCase(assertEqual "(- 2 1)" (Right (AInt 1)) (builtinSub [AInt 2, AInt 1])),
+    TestCase(assertEqual "(- 6 2 1)" (Right (AInt 3)) (builtinSub [AInt 6, AInt 2, AInt 1]))
     ]
 
 testBuiltinsMul :: Test
 testBuiltinsMul = TestList[
-    TestCase(assertEqual "2 * 2" (Just (AInt 4)) (apply "*" [AInt 2, AInt 2])),
-    TestCase(assertEqual "2 * 2 * 3" (Just (AInt 12)) (apply "*" [AInt 2, AInt 2, AInt 3]))
+    TestCase(assertEqual "(* 2 2)" (Right (AInt 4)) (builtinMul [AInt 2, AInt 2])),
+    TestCase(assertEqual "(* 2 2 3)" (Right (AInt 12)) (builtinMul [AInt 2, AInt 2, AInt 3]))
     ]
 
+testBuiltinsDiv :: Test
+testBuiltinsDiv = TestList[
+    TestCase(assertEqual "(div 2 2)" (Right (AInt 1)) (builtinDiv [AInt 2, AInt 2])),
+    TestCase(assertEqual "(div 8 2 2)" (Left "expected exactly 2 integers") (builtinDiv [AInt 2, AInt 2, AInt 3]))
+    ]
+
+testBuiltinsMod :: Test
+testBuiltinsMod = TestList[
+    TestCase(assertEqual "(mod 5 2)" (Right (AInt 1)) (builtinMod [AInt 5, AInt 2]))
+    ]
+
+testBuiltins :: Test
+testBuiltins = TestList[
+    testBuiltinsAdd,
+    testBuiltinsSub,
+    testBuiltinsMul,
+    testBuiltinsDiv,
+    testBuiltinsMod
+    ]
 
 tests :: Test
 tests = TestList[
-    testBuiltinsAdd,
-    testBuiltinsSub,
-    testBuiltinsMul
+    testBuiltins
     ]
 
 main :: IO Counts
