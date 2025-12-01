5bcafa7454127c1842568b71f238b18e7188f49e
Kerwanc <kerwan.c@gmail.com>
2025-12-01 10:23:24 +0100

    feat(list-main): added the eval function to the main for testing

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ module Main (main) where
 import SExprToAST
 import ParserSExpr
 import Parser
+import Eval(evalAST)
+import qualified Data.Map as Map
 
 main :: IO ()
 main =
@@ -18,6 +20,9 @@ main =
       Right (sexpr, _) ->
         case sexprToAST sexpr of
             Left err -> putStrLn ("Could not convert SExpr to AST" ++ err)
-            Right ast -> print ast
+            Right ast -> 
+              case evalAST Map.empty ast of
+                Left err -> putStrLn ("Evaluation error: " ++ err)
+                Right result -> putStrLn ("Evaluation result: " ++ show (snd result)) 
     where
-      string = "(1 \"hello\" 1)"
+      string = "(* 1 1 1)"
