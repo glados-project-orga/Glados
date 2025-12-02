35301f8bc3521094fe39a69d35a8687d1b73db45
Kerwanc <kerwan.c@gmail.com>
2025-12-02 14:57:36 +0100

    feat(lisp-ast): added ABool Bool to the ast datatype

--- src/ast/Data.hs
+++ src/ast/Data.hs
@@ -17,6 +17,7 @@ data SExpr = SInt Int
 
 data Ast = AInt Int
          | ASymbol String
+         | ABool Bool
          | AList [Ast]
          | ACall Ast [Ast]
          | ADefine String Ast
@@ -26,6 +27,7 @@ data Ast = AInt Int
 instance Show Ast where
     show (AInt n) = show n
     show (ASymbol s) = s
+    show (ABool b) = show b
     show (AList list) = (formatList list)
     show (ACall f args) = "(" ++ show f ++ " " ++ concatMap (\x -> show x ++ " ") args ++ ")"
     show (ADefine name expr) = "(define " ++ name ++ " " ++ show expr ++ ")"
