d833242392f4899857b17a03fccf747960a804d4
4urel PLIY4 <aurel.pliya@epitech.eu>
2025-12-08 22:46:36 +0100

    fix: cut too long evalAst function

--- src/ast/Eval.hs
+++ src/ast/Eval.hs
@@ -10,11 +10,66 @@ module Eval (
     Env
 ) where
 
+
 import Data(Ast(..))
 import Builtins(apply)
 
 import qualified Data.Map as Map
 type Env = Map.Map String Ast
+
+applyBuiltin :: Env -> [Ast] -> ([Ast] -> Either String Ast) -> Either String (Env, Ast)
+applyBuiltin env args f =
+  case traverse (fmap snd . evalAST env) args of
+        Left err -> Left err
+        Right vals ->
+          case f vals of
+            Left err -> Left err
+            Right val -> Right (env, val)
+
+
+applyLambda:: Env -> [String] -> Ast -> [Ast] -> Either String (Env, Ast)
+applyLambda env argss body args =
+  if length argss == length args
+    then case traverse (fmap snd . evalAST env) args of
+      Left err     -> Left err
+      Right argvals ->
+        let newEnv = Map.union (Map.fromList (zip argss argvals)) env
+        in evalAST newEnv body
+    else Left "Invalid number of arguments"
+
+
+evalASTAcall1 :: Env -> String -> [Ast] -> Either String (Env, Ast)
+evalASTAcall1 env name args =
+  case Map.lookup name apply of
+
+    Just f -> applyBuiltin env args f
+
+    Nothing ->
+        case Map.lookup name env of
+          Just (ALambda argss body) -> applyLambda env argss body args
+
+          Just other ->
+            Left ("Trying to call non-function: " ++ show other)
+  
+          Nothing ->
+            Left ("Unknown function: " ++ name)
+
+
+evalASTAcall2 :: Env -> Ast -> [Ast] -> Either String (Env, Ast)
+evalASTAcall2 env ffn args =
+  case evalAST env ffn of
+    Left err -> Left err
+
+    Right(_, ALambda argss body) -> applyLambda env argss body args
+
+    Right(_, ASymbol name) ->
+      case Map.lookup name apply of
+        Nothing -> Left ("Unknown function: " ++ name)
+        Just f -> applyBuiltin env args f
+
+    Right (_, other) ->
+      Left ("Trying to call non-function: " ++ show other)
+
 
 evalAST :: Env -> Ast -> Either String (Env, Ast)
 evalAST env (AInt li) = Right (env, AInt li)
@@ -45,60 +100,6 @@ evalAST env (ACall (ASymbol "if") [cond, thenBranch]) =
     Right (_, ABool False) -> Right (env, AVoid)
     Right (_, other) -> Left ("if condition must be a boolean, and not: " ++ show other)
 
-evalAST env (ACall (ASymbol name) args) =
-
-  case Map.lookup name apply of
-    Just f ->
-      case traverse (fmap snd . evalAST env) args of
-        Left err -> Left err
-        Right vals ->
-          case f vals of
-            Left err -> Left err
-            Right val -> Right (env, val)
-  
-    Nothing ->
-        case Map.lookup name env of
-          Just (ALambda argss body) ->
-            if length argss == length args
-              then case traverse (fmap snd . evalAST env) args of
-                Left err     -> Left err
-                Right argvals ->
-                  let newEnv = Map.union (Map.fromList (zip argss argvals)) env
-                  in evalAST newEnv body
-              else Left "Invalid number of arguments"
-
-          Just other ->
-            Left ("Trying to call non-function: " ++ show other)
-
-          Nothing ->
-            Left ("Unknown function: " ++ name)
-
-evalAST env (ACall ffn args) =
-
-  case evalAST env ffn of
-    Left err -> Left err
-
-    Right(_, ALambda argss body) ->
-      if length argss == length args
-        then case traverse (fmap snd . evalAST env) args of
-          Left err -> Left err
-          Right argvals ->
-            let newEnv = Map.union(Map.fromList (zip argss argvals)) env
-            in evalAST newEnv body
-      else Left "Invalid number of arguments"
-
-    Right(_, ASymbol name) ->
-      case Map.lookup name apply of
-        Nothing -> Left ("Unknown function: " ++ name)
-        Just f ->
-          case traverse (fmap snd . evalAST env) args of
-            Left err -> Left err
-            Right vals ->
-              case f vals of
-                Left err -> Left err
-                Right val -> Right (env, val)
-
-    Right (_, other) ->
-      Left ("Trying to call non-function: " ++ show other)
-
+evalAST env (ACall (ASymbol name) args) = evalASTAcall1 env name args
+evalAST env (ACall ffn args) = evalASTAcall2 env ffn args
 evalAST _ baned = Left ("No correct Ast format: " ++ show baned)
