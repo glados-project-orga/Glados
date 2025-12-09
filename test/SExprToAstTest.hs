d2c7ca001e97e9208e072668b1b466e04c8a13fb
celianrag <celian.raguin@epitech.eu>
2025-12-09 01:18:13 +0100

    feat(TestSexprToAST): adding lambda unit test for sexpr to ast

--- test/SExprToAstTest.hs
+++ test/SExprToAstTest.hs
@@ -42,7 +42,19 @@ testSExprACalls :: Test
 testSExprACalls = TestList [
     TestCase(assertEqual "SList to ACall" (Right (ACall (ASymbol "+") [AInt 5, AInt 6])) (sexprToAST (SList [SSymbol "+", SInt 5, SInt 6]))),
     TestCase(assertEqual "Nested ACall" (Right (ACall (ASymbol "+") [ACall (ASymbol "*") [AInt 2, AInt 3], AInt 4]))
-        (sexprToAST (SList [SSymbol "+", SList [SSymbol "*", SInt 2, SInt 3], SInt 4])))
+        (sexprToAST (SList [SSymbol "+", SList [SSymbol "*", SInt 2, SInt 3], SInt 4]))),
+    TestCase(assertEqual "Call with invalid function" (Left ("Unsupported SExpr form")) (sexprToAST (SList [SList [], SInt 1]))),
+    TestCase(assertEqual "Call with empty list" (Left ("Unsupported SExpr form")) (sexprToAST (SList [SSymbol "+", SList []]))),
+    TestCase(assertEqual "Call with empty list" (Left ("Unsupported SExpr form")) (sexprToAST (SList [SSymbol "+", SList []])))
+    ]
+
+testSExprALambda :: Test
+testSExprALambda = TestList [
+    TestCase (assertEqual "SList to ALambda"
+    (Right (ACall (ALambda ["x"]
+        (ASymbol "x")) [AInt 1])) (sexprToAST (SList [ SList [SSymbol "lambda", SList [SSymbol "x"], SSymbol "x"], SInt 1 ]))),
+    TestCase (assertEqual "Lambda with invalid args"
+        (Left "Unsupported SExpr form") (sexprToAST (SList [ SList [SSymbol "lambda", SList [SSymbol "x"], SSymbol "x"], SList [] ])))
     ]
 
 testSExprAList :: Test
@@ -52,4 +64,11 @@ testSExprAList = TestList [
     ]
 
 testSExpr :: Test
-testSExpr = TestList[testSExprACalls, testSExprAint, testSExprASymbol, testSExprADefine, testSExprAList]
+testSExpr = TestList[
+    testSExprACalls,
+    testSExprAint,
+    testSExprASymbol,
+    testSExprADefine,
+    testSExprAList,
+    testSExprALambda
+    ]
