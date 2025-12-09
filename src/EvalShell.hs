b40c1efdbf428052d5ce753a877264b711171de1
luigi gomes <luigi.gomes@epitech.eu>
2025-12-09 11:04:48 +0100

    fix(split function): fixing the coding style and function comprehension by only using pattern matching

--- src/EvalShell.hs
+++ src/EvalShell.hs
@@ -24,15 +24,14 @@ splitSExprs str = splitAux str 0 "" []
 splitAux :: String -> Int -> String -> [String] -> [String]
 splitAux [] _ "" acc = reverse acc
 splitAux [] _ buf acc = reverse (buf:acc)
-splitAux (c:cs) nbPar buf acc
-    | c == '('  = splitAux cs (nbPar + 1) (buf ++ [c]) acc
-    | c == ')'  = if (nbPar - 1) == 0
-        then splitAux cs 0 "" ((buf ++ [c]) : acc)
-        else splitAux cs (nbPar - 1) (buf ++ [c]) acc
-    | nbPar == 0 && c == '\n' && not (null buf) =
-                    splitAux cs nbPar "" ((buf ++ [c]):acc)
-    | c == '\n' || c == '\t' = splitAux cs nbPar buf acc
-    | otherwise = splitAux cs nbPar (buf ++ [c]) acc
+splitAux ('(':cs) nbPar buf acc = splitAux cs (nbPar + 1) (buf ++ ['(']) acc
+splitAux (')':cs) nbPar buf acc = if (nbPar - 1) == 0
+    then splitAux cs 0 "" ((buf ++ [')']):acc)
+    else splitAux cs (nbPar - 1) (buf ++ [')']) acc
+splitAux ('\n':cs) 0 buf acc | not (null buf) =
+    splitAux cs 0 "" ((buf ++ ['\n']):acc)
+splitAux ('\n':cs) nbPar buf acc = splitAux cs nbPar buf acc
+splitAux (c:cs) nbPar buf acc = splitAux cs nbPar (buf ++ [c]) acc
 
 evalAll :: [String] -> Env -> IO ()
 evalAll [] _ = exitWith ExitSuccess
@@ -41,6 +40,10 @@ evalAll (input:expr) env =
         Left err -> putStrLn err >> exitWith (ExitFailure 84)
         Right (newEnv, AVoid) -> evalAll expr newEnv
         Right (newEnv, ast) -> print ast >> evalAll expr newEnv
+
+printSexpr :: [String] -> IO ()
+printSexpr [] = return ()
+printSexpr (str:rest) = putStrLn str >> printSexpr rest
 
 evalRedirShell :: String -> Env -> IO ()
 evalRedirShell input env = evalAll (splitSExprs input) env
