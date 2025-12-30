7eb438456f169a0fd38f635f49a6a1b5702cc63e
4urel PLIY4 <aurel.pliya@epitech.eu>
2025-12-30 09:42:22 +0100

    feat: add heap, instructions, frames and VM state data structures

--- vm/Data2.hs
+++ vm/Data2.hs
@@ -53,3 +53,61 @@ type Stack = [Value]
 type Insts = [Action]
 type Args  = [Value]
 type Function = [Action]
+
+
+type Handle = Int
+
+data HeapValue = HObject (Map.Map String Value)
+               |HArray [Value] 
+               deriving(Show, Eq)
+
+type Heap = Map.Map Handle HeapValue
+
+
+data Instr = IConstInt Int
+           | ILoadInt Int
+           | IStoreInt Int
+           | IAddInt
+           | ISubInt
+           | IMulInt
+           | IDivInt
+           | IPop
+           | IDup
+   
+           | IGoto Int
+           | IIfEq Int
+           | IIfGt Int
+           | IIfLt Int
+           | IIfICmpGt Int
+           | IIfICmpLt Int
+   
+           | IInvokeStatic String
+           | IInvokeVirtual String
+           | IInvokeSpecial String
+           | IReturn
+           | IReturnInt
+   
+           | INew String
+           | IGetField String
+           | IPutField String
+   
+           | INewArray
+           | IALoad
+           | IAStore
+           | IArrayLength
+           deriving (Show, Eq)
+
+data Frame = Frame
+    { fLocals :: [Value]
+    , fIP     :: Int
+    } deriving (Show)
+
+
+data VMState = VMState
+        {  stack  :: Stack,
+           locals :: [Value],
+           ip     :: Int,
+           code   :: [Instr],
+           heap   :: Heap,
+           frames :: [Frame]
+        } deriving(Show)
