00854a09465d9e10b99e6a9ee59e5f9e8d6eb013
4urel PLIY4 <aurel.pliya@epitech.eu>
2026-01-02 20:28:48 +0100

    Feat: Add implemntation of Builtin Instruction (IAdd, IMul, IDiv and ISub)

--- vm/vmState.hs
+++ vm/vmState.hs
@@ -36,5 +36,37 @@ exec st@VMState{stack, locals, ip, code, heap, frames} =
                                 Nothing -> Left ("Invalid local index: " ++ show n) 
                                 Just _ -> 
                                     let newLocals = locals V.// [(n, first)] 
-                                    in Right st {ip = ip + 1, stack = rest, locals = newLocals}            
+                                    in Right st {ip = ip + 1, stack = rest, locals = newLocals}
+                                IMulInt ->
+                    case stack of
+                        [] -> Left "Empty Stack  in IMulInt"
+                        (VInt i2: VInt i1: rest) -> 
+                            let newRet = i1 * i2
+                            in Right st {ip = ip + 1, stack = (VInt newRet:rest)}
+                        _ -> Left "Type error or insufficient stack in IMulInt"
+
+                IDivInt ->
+                    case stack of
+                        [] -> Left "Empty Stack  in IDivInt"
+                        (VInt i2: VInt i1: rest) -> 
+                            let newRet = i1 `div` i2
+                            in Right st {ip = ip + 1, stack = (VInt newRet:rest)}
+                        _ -> Left "Type error or insufficient stack in IDivInt"
+
+                ISubInt ->
+                    case stack of
+                        [] -> Left "Empty Stack  in ISubInt"
+                        (VInt i2: VInt i1: rest) -> 
+                            let newRet = i1 - i2
+                            in Right st {ip = ip + 1, stack = (VInt newRet:rest)}
+                        _ -> Left "Type error or insufficient stack in ISubInt" 
+                
+                IAddInt ->
+                    case stack of
+                        [] -> Left "Empty Stack  in IAddInt"
+                        (VInt i2: VInt i1: rest) -> 
+                            let newRet = i1 + i2
+                            in Right st {ip = ip + 1, stack = (VInt newRet:rest)}
+                        _ -> Left "Type error or insufficient stack in IAddInt"
+        
                 _ -> Left ("Instruction not implemented: " ++ show instr)
