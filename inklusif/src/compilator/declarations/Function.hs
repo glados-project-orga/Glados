013f9127a8483da30ad42a6454d745c8b234479c
celianrag <celian.raguin@epitech.eu>
2026-01-03 02:15:16 +0100

    fix(function.hs/compilator): adding good number of arg for compile function

--- inklusif/src/compilator/declarations/Function.hs
+++ inklusif/src/compilator/declarations/Function.hs
@@ -2,4 +2,4 @@ module Function (compileFunction) where
 import ast AST (FunctionDecl(..))
 
 compileFunction :: FunctionDecl -> (ConstantPool, Bytecode)
-compileFunction _ _ _ = ([], [])
+compileFunction _ _ _ _ _ = ([], [])
