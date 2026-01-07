4bdee4cb5dff3808a55265c641063aa999afb356
4urel PLIY4 <aurel.pliya@epitech.eu>
2026-01-07 22:56:22 +0100

    feat: Add comparaison instruction (IIfICmpGt, IIfICmpLt, IIfACmpEq, IIfACmpNe)

--- vm/src/ComparInstr.hs
+++ vm/src/ComparInstr.hs
@@ -8,7 +8,6 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 
-
 import Data
 import Data.Bits
 import HeapInstr
@@ -17,30 +16,70 @@ import qualified Data.Vector as V
 
 
 compIfEq :: Int -> VMState -> Either String VMState
-compIfEq offset st@VMState{stack, ip} =
+compIfEq n st@VMState{stack, ip} =
     case stack of
         (VInt v : rest) ->
             if v == 0
-                then Right st { ip = ip + offset, stack = rest }
+                then Right st { ip = ip + n, stack = rest }
                 else Right st { ip = ip + 1, stack = rest }
         _ -> Left "IIfEq: expected int on stack"
 
 
 compIfGt :: Int -> VMState -> Either String VMState
-compIfGt offset st@VMState{stack, ip} =
+compIfGt n st@VMState{stack, ip} =
     case stack of
         (VInt v : rest) ->
             if v > 0
-                then Right st { ip = ip + offset, stack = rest }
+                then Right st { ip = ip + n, stack = rest }
                 else Right st { ip = ip + 1, stack = rest }
         _ -> Left "IIfGt: expected int on stack"
 
 
 compIfLt :: Int -> VMState -> Either String VMState
-compIfLt offset st@VMState{stack, ip} =
+compIfLt n st@VMState{stack, ip} =
     case stack of
         (VInt v : rest) ->
             if v < 0
-                then Right st { ip = ip + offset, stack = rest }
+                then Right st { ip = ip + n, stack = rest }
                 else Right st { ip = ip + 1, stack = rest }
         _ -> Left "IIfLt: expected int on stack"
+
+
+compIfICmpGt :: Int -> VMState -> Either String VMState
+compIfICmpGt n st@VMState{stack, ip} =
+    case stack of
+        (VInt b : VInt a : rest) ->
+            if a > b
+                then Right st { ip = ip + n, stack = rest }
+                else Right st { ip = ip + 1, stack = rest }
+        _ -> Left "IIfICmpGt: expected two ints on stack"
+
+
+compIfICmpLt :: Int -> VMState -> Either String VMState
+compIfICmpLt n st@VMState{stack, ip} =
+    case stack of
+        (VInt b : VInt a : rest) ->
+            if a < b
+                then Right st { ip = ip + n, stack = rest }
+                else Right st { ip = ip + 1, stack = rest }
+        _ -> Left "IIfICmpLt: expected two ints on stack"
+
+
+compIfACmpEq :: Int -> VMState -> Either String VMState
+compIfACmpEq n st@VMState{stack, ip} =
+    case stack of
+        (VInt b : VInt a : rest) ->
+            if a == b
+                then Right st { ip = ip + n, stack = rest }
+                else Right st { ip = ip + 1, stack = rest }
+        _ -> Left "IIfACmpEq: expected two refs on stack"
+
+
+compIfACmpNe :: Int -> VMState -> Either String VMState
+compIfACmpNe n st@VMState{stack, ip} =
+    case stack of
+        (VInt b : VInt a : rest) ->
+            if a /= b
+                then Right st { ip = ip + n, stack = rest }
+                else Right st { ip = ip + 1, stack = rest }
+        _ -> Left "IIfACmpNe: expected two refs on stack"
