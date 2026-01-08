fe07baeb27aff26cb514308d0e3e6cf8da691b7d
celianrag <celian.raguin@epitech.eu>
2026-01-08 22:33:03 +0100

    feat(compiler_utils): adding tools to append data to binary

--- inklusif/src/compilator/utils/CompilerTools.hs
+++ inklusif/src/compilator/utils/CompilerTools.hs
@@ -1,6 +1,19 @@
-module CompilerTools (appendProgramBinaries) where
-import CompilerTypes(ProgramBinary)
+module CompilerTools (
+    appendProgramBinaries,
+    appendToHeader,
+    appendToDefines,
+    appendToBody) where
+import CompilerTypes(ProgramBinary, ConstantPool, Defines, Bytecode)
 
 appendProgramBinaries :: ProgramBinary -> ProgramBinary -> ProgramBinary
 appendProgramBinaries (head1, def1, body1) (head2, def2, body2) =
     (head1 ++ head2, def1 ++ def2, body1 ++ body2)
+
+appendToHeader :: ProgramBinary -> ConstantPool -> ProgramBinary
+appendToHeader (header, def, body) newHead = (header ++ newHead, def, body)
+
+appendToDefines :: ProgramBinary -> Defines -> ProgramBinary
+appendToDefines (header, def, body) newDef = (header, def ++ newDef, body)
+
+appendToBody :: ProgramBinary -> Bytecode -> ProgramBinary
+appendToBody (header, def, body) newBody = (header, def, body ++ newBody)
