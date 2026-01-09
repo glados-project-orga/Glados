4533f49c3397c9571754f8772c7143f5f179ac5b
celianrag <celian.raguin@epitech.eu>
2026-01-09 23:28:00 +0100

    feat(functions): adding error handling if redclaring funciton that already exist

--- inklusif/src/compilator/declarations/Function.hs
+++ inklusif/src/compilator/declarations/Function.hs
@@ -3,14 +3,16 @@ import Ast (FunctionDecl(..))
 import Statements (compileStatements)
 import CompilerTypes (CompilerData)
 import CompilerTools (appendBody)
+import FunctionUtils (searchFunctions)
+
 closeFunction :: Either String CompilerData -> Either String CompilerData
 closeFunction (Left err) = Left err
 closeFunction (Right prog) = Right (appendBody prog ["}\n"])
 
 compileFunction :: FunctionDecl -> CompilerData-> Either String CompilerData
 compileFunction (FunctionDecl _ name _ _ statements) (header, defs, body, _)
-    = new_prog
-        where
-          new_prog = closeFunction uncomplete_prog
-          uncomplete_prog = compileStatements statements (Right open_fun)
-          open_fun = (header, defs, body ++ ["fun" ++ " " ++ name ++ " {\n"], [])
+    | searchFunctions name defs /= Nothing = Left ("Function " ++ name ++ " is already defined.")
+    | otherwise = new_prog
+        where new_prog = closeFunction uncomplete_prog
+              uncomplete_prog = compileStatements statements (Right open_fun)
+              open_fun = (header, defs, body ++ ["fun" ++ " " ++ name ++ " {\n"], [])
