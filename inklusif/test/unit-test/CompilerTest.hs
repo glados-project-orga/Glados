cc2dbc77a3445b65989e9459598636642dc7b2f6
celianrag <celian.raguin@epitech.eu>
2026-01-09 21:32:37 +0100

    feat(unit-tests) adding base unit-tests for compiler

---
+++ inklusif/test/unit-test/CompilerTest.hs
@@ -0,0 +1,39 @@
+{- 
+-- EPITECH PROJECT, 2025
+-- epi-repo
+-- File description:
+-- CompilerTest.hs
+-}
+
+module CompilerTest (
+    testCompiler
+) where
+import CompilerMain (compilerMain)
+import Test.HUnit
+import Ast (Declaration(..), FunctionDecl(..), SourcePos(..), Type(..))
+
+testCompilerMain :: Test
+testCompilerMain = TestList[
+    TestCase (assertEqual "compilerMain with empty AST" 
+        (Right ([], ([], [], [], []), [], []))
+        (compilerMain [] (Right ([], ([], [], [], []), [], [])))
+    ),
+    TestCase (assertEqual "compilerMain with one function declaration" 
+        (Right ([], ([testFunc], [], [], []), ["fun foo {\n","}\n"], [])) 
+        (compilerMain [Function testFunc] 
+            (Right ([], ([], [], [], []), [], [])))
+    )
+    ]
+    where testFunc = FunctionDecl (SourcePos 0 0) "foo" [] VoidType []
+
+-- testFoundInt :: Test
+-- testFoundInt = TestList[
+--     TestCase(assertEqual "foundInt AInt 5" (Right 5) (foundInt (AInt 5))),
+--     TestCase(assertEqual "foundInt ASymbol foo" (Left "foo is not a number") (foundInt (ASymbol "foo")))
+--     ]
+
+
+testCompiler :: Test
+testCompiler = TestList[
+    testCompilerMain
+    ]
