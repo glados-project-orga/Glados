19ee29023225cbe40a8a7a177280690887d7e6b9
luigi gomes <luigi.gomes@epitech.eu>
2026-01-12 14:26:08 +0100

    fix(Struct file): removing useless comments

--- inklusif/src/parser/StructParser.hs
+++ inklusif/src/parser/StructParser.hs
@@ -12,16 +12,7 @@ module StructParser (
 import Ast
 import Parser
 import Control.Applicative
--- import Debug.Trace (trace)
 import StatementParser (parseType)
-
--- traceInput :: String -> Parser a -> Parser a
--- traceInput label (Parser p) =
---     Parser $ \st ->
---         trace
---             (label ++ " | next input = " ++ take 40 (input st)
---              )
---             (p st)
 
 parseStructField :: Parser StructField
 parseStructField =
