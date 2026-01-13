9a99cde6a3cc56d1e2384174efeba64555693c5a
Hajar <hajar.ahazzam@epitech.eu>
2026-01-13 17:57:41 +0100

    feat(expr): extend literal compilation (int, float, bool, char)

--- inklusif/src/compilator/expressions/src/Literal.hs
+++ inklusif/src/compilator/expressions/src/Literal.hs
@@ -5,15 +5,23 @@
 -- expr
 -}
 
-module Literal (compileLiteralExpr) where
+module Literal (compileLiteralExpr, compileLiteralExprT) where
 
-import Ast (Literal(..))
+import Ast (Literal(..), Type(..))
 import CompilerTypes (CompilerData)
 import CompilerTools (appendBody)
 
 compileLiteralExpr :: Literal -> CompilerData -> Either String CompilerData
-compileLiteralExpr (IntLit n) prog = Right (appendBody prog (emitPushInt n))
-compileLiteralExpr _ _ = Left "Literal not implemented yet"
+compileLiteralExpr lit prog = snd <$> compileLiteralExprT lit prog
+
+compileLiteralExprT :: Literal -> CompilerData -> Either String (Type, CompilerData)
+compileLiteralExprT (IntLit n) prog = Right (IntType, appendBody prog (emitPushInt n))
+compileLiteralExprT (FloatLit x) prog = Right (FloatType, appendBody prog (emitPushFloat x))
+compileLiteralExprT (BoolLit True) prog = Right (BoolType, appendBody prog ["iconst_1"])
+compileLiteralExprT (BoolLit False) prog = Right (BoolType, appendBody prog ["iconst_0"])
+compileLiteralExprT (CharLit c) prog = Right (CharType, appendBody prog (emitPushInt (fromEnum c)))
+compileLiteralExprT (StringLit s) prog = Right (StringType, appendBody prog ["ldc \"" ++ s ++ "\""])
+compileLiteralExprT _ _ = Left "Literal not implemented yet"
 
 emitPushInt :: Int -> [String]
 emitPushInt (-1) = ["iconst_m1"]
@@ -27,3 +35,9 @@ emitPushInt n
   | n >= (-128) && n <= 127       = ["bipush " ++ show n]
   | n >= (-32768) && n <= 32767   = ["sipush " ++ show n]
   | otherwise                     = ["ldc " ++ show n]
+
+emitPushFloat :: Double -> [String]
+emitPushFloat 0.0 = ["fconst_0"]
+emitPushFloat 1.0 = ["fconst_1"]
+emitPushFloat 2.0 = ["fconst_2"]
+emitPushFloat x   = ["ldc " ++ show x]
