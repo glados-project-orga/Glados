730aa9634ff01879c95d336993ffdd70e0ffff4c
Hajar <hajar.ahazzam@epitech.eu>
2026-01-13 17:19:56 +0100

    feat(expr): add type checking for function calls

--- inklusif/src/compilator/expressions/src/Call.hs
+++ inklusif/src/compilator/expressions/src/Call.hs
@@ -5,32 +5,91 @@
 -- Call.hs
 -}
 
-module Call (compileCallExpr) where
+module Call (compileCallExpr, compileCallExprT) where
 
-import Ast (Expr(..), CallExpr(..), FunctionDecl(..))
+import Ast (Expr(..),
+            CallExpr(..),
+            FunctionDecl(..),
+            Type(..),
+            Parameter(..))
 import CompilerTypes (CompilerData)
 import Lookup (getFunctions, findFunction)
 import Arity (checkArity)
-import ExprArgs (compileArgs)
+import ExprArgs (compileArgs, compileArgsT)
 import CompilerTools (appendBody)
+import TypeCheck (expectType)
 
 emitCall :: String -> [String]
 emitCall fname = ["invokestatic " ++ fname]
 
 compileCallInstr :: String -> CompilerData -> CompilerData
-compileCallInstr fname prog = (appendBody prog (emitCall fname))
+compileCallInstr fname prog = appendBody prog (emitCall fname)
+
+bindE :: Either String a -> (a -> Either String b) -> Either String b
+bindE (Left err) _ = Left err
+bindE (Right x) f  = f x
+
+thenE :: Either String a -> Either String b -> Either String b
+thenE (Left err) _  = Left err
+thenE (Right _) nxt = nxt
+
 compileCallExpr
   :: (Expr -> CompilerData -> Either String CompilerData)
   -> CallExpr
   -> CompilerData
   -> Either String CompilerData
 compileCallExpr rec (CallExpr fname args) prog@(_, defs, _, _) =
-  case findFunction fname (getFunctions defs) of
-    Nothing -> Left ("Undefined function: " ++ fname)
-    Just fdecl ->
-      case checkArity fname (funcParams fdecl) args of
-        Left err -> Left err
-        Right () ->
-          case compileArgs rec args prog of
-            Left err -> Left err
-            Right pArgs -> Right (compileCallInstr fname pArgs)
+  compileCallFound rec fname args prog (findFunction fname (getFunctions defs))
+
+compileCallFound
+  :: (Expr -> CompilerData -> Either String CompilerData)
+  -> String -> [Expr] -> CompilerData
+  -> Maybe FunctionDecl
+  -> Either String CompilerData
+compileCallFound _ fname _ _ Nothing =
+  Left ("Undefined function: " ++ fname)
+compileCallFound rec fname args prog (Just fdecl) =
+  thenE (checkArity fname (funcParams fdecl) args)
+    (bindE (compileArgs rec args prog) (\pArgs ->
+      Right (compileCallInstr fname pArgs)))
+
+compileCallExprT
+  :: (Expr -> CompilerData -> Either String (Type, CompilerData))
+  -> CallExpr
+  -> CompilerData
+  -> Either String (Type, CompilerData)
+compileCallExprT rec (CallExpr fname args) prog@(_, defs, _, _) =
+  compileCallFoundT rec fname args prog (findFunction fname (getFunctions defs))
+
+compileCallFoundT
+  :: (Expr -> CompilerData -> Either String (Type, CompilerData))
+  -> String -> [Expr] -> CompilerData
+  -> Maybe FunctionDecl
+  -> Either String (Type, CompilerData)
+compileCallFoundT _ fname _ _ Nothing =
+  Left ("Undefined function: " ++ fname)
+compileCallFoundT rec fname args prog (Just fdecl) =
+  let expected = map paramType (funcParams fdecl)
+      retT     = funcReturnType fdecl
+  in thenE (checkArity fname (funcParams fdecl) args)
+       (bindE (compileArgsT rec args prog) (\(given, pArgs) ->
+         thenE (checkArgTypes fname expected given)
+           (Right (retT, compileCallInstr fname pArgs))))
+
+checkArgTypes :: String -> [Type] -> [Type] -> Either String ()
+checkArgTypes fname expected given =
+  prefixErr ("Type error in call to " ++ fname ++ ": ") (checkPairs 1 expected given)
+
+checkPairs :: Int -> [Type] -> [Type] -> Either String ()
+checkPairs _ [] [] = Right ()
+checkPairs i (e:es) (g:gs) =
+  thenE (prefixArg i (expectType e g)) (checkPairs (i + 1) es gs)
+checkPairs _ _ _ = Left "arity mismatch (unexpected)"
+
+prefixArg :: Int -> Either String () -> Either String ()
+prefixArg _ ok@(Right ()) = ok
+prefixArg i (Left err) = Left ("argument " ++ show i ++ " " ++ err)
+
+prefixErr :: String -> Either String a -> Either String a
+prefixErr _ ok@(Right _) = ok
+prefixErr p (Left e) = Left (p ++ e)
