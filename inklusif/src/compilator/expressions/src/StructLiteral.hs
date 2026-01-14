4634840b407265c974991d49915759f384dbc4a1
Hajar <hajar.ahazzam@epitech.eu>
2026-01-14 21:41:10 +0100

    chore(expr): move expression helpers to utils folder

--- inklusif/src/compilator/expressions/src/StructLiteral.hs
+++
@@ -1,6 +0,0 @@
-module StructLiteral (compileStructLiteral) where
-import CompilerTypes (CompilerData)
-import Ast (Expr)
-
-compileStructLiteral :: String -> [(String, Expr)] -> CompilerData -> Either String CompilerData
-compileStructLiteral _ _ prog = Right prog
