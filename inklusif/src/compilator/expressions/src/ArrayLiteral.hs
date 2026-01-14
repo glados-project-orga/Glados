387db4aa41fe48808190ba10507ae6ad8b5fee79
celianrag <celian.raguin@epitech.eu>
2026-01-14 22:22:53 +0100

    feat(compilator) adding first array litteral

--- inklusif/src/compilator/expressions/src/ArrayLiteral.hs
+++ inklusif/src/compilator/expressions/src/ArrayLiteral.hs
@@ -1,6 +1,15 @@
 module ArrayLiteral (compileArrayLiteral) where
 import CompilerTypes (CompilerData)
+import CompilerTools (appendBody)
+import Expr (compileExpr)
 import Ast (Expr)
 
+compileOneCell :: [Expr] -> Int -> CompilerData -> Either String CompilerData
+compileOneCell [] _ prog = Right prog
+compileOneCell (ex:exs) index prog = compileExpr ex addedIndexProg
+    >>= (\new_prog -> compileOneCell exs (index + 1) new_prog)
+        where addedIndexProg = appendBody prog [("iconst " ++ show index)]
+
 compileArrayLiteral :: [Expr] -> CompilerData -> Either String CompilerData
-compileArrayLiteral _ prog = Right prog
+compileArrayLiteral [] prog = Right prog
+compileArrayLiteral exprs prog = compileOneCell exprs 0 prog
