31ec123ce28bd28d970d1dd36ddd7f5d7207b688
celianrag <celian.raguin@epitech.eu>
2026-01-14 14:10:54 +0100

    feat(typedef): adding typedef compilation

--- inklusif/src/compilator/declarations/Typedef.hs
+++ inklusif/src/compilator/declarations/Typedef.hs
@@ -1,6 +1,15 @@
 module Typedef (compileTypedef) where
-import Ast (TypedefDecl(..))
+import Ast (TypedefDecl(..), Declaration(..))
 import CompilerTypes (CompilerData)
+import CompilerError (errPos)
+import CompilerTools (appendDefines)
+
+typeNameExists :: String -> [TypedefDecl] -> Bool
+typeNameExists searched typedefs =
+    any (\(TypedefDecl _ _ name) -> name == searched) typedefs
 
 compileTypedef :: TypedefDecl  -> CompilerData -> Either String CompilerData
-compileTypedef _ prog = Right prog
+compileTypedef typedef@(TypedefDecl pos _ name) prog@(_, (_, _, _, typedefs), _, _)
+    | typeNameExists name typedefs =
+        Left ((errPos pos) ++ "Typedef " ++ name ++ " is already defined.")
+    | otherwise = Right (appendDefines prog [(Typedef typedef)])
