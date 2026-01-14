30dfb303cb7bd96176c6da137c881194b95be015
Hajar <hajar.ahazzam@epitech.eu>
2026-01-14 22:38:12 +0100

    refactor(expr): empty expressions

---
+++ inklusif/src/compilator/expressions/src/ArrayIndex.hs
@@ -0,0 +1,7 @@
+module ArrayIndex (compileArrayIndex) where
+
+import CompilerTypes (CompilerData)
+import Ast (ArrayIndexExpr)
+
+compileArrayIndex :: ArrayIndexExpr -> CompilerData -> Either String CompilerData
+compileArrayIndex _ prog = Right prog
