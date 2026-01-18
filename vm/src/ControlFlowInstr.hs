b421cfb7eb8ca7879ded80466732e8a5c52a3581
Kerwanc <kerwan.c@gmail.com>
2026-01-18 01:29:37 +0100

    feat(vm-instr): implemented creturn instr

--- vm/src/ControlFlowInstr.hs
+++ vm/src/ControlFlowInstr.hs
@@ -15,6 +15,7 @@ module ControlFlowInstr (
     controlFlowReturnDouble,
     controlFlowReturnFloat,
     controlFlowReturnLong,
+    controlFlowReturnChar,
     controlFlowInvokeStatic
 ) where
 
@@ -130,6 +131,28 @@ controlFlowReturnLong st@VMState{stack, functions, currentFunc, frames} =
                     }
         _ -> Left "IReturnLong expects an long on top of the stack"
 
+
+controlFlowReturnChar :: VMState -> Either String VMState
+controlFlowReturnChar st@VMState{stack, functions, currentFunc, frames} =
+    case stack of
+        [] -> Left "Stack underflow in IReturnChar"
+        (VChar returnValue:restStack) ->
+            case frames of
+                [] ->
+                    case Map.lookup currentFunc functions of
+                        Nothing -> Left ("Function not found: " ++ currentFunc)
+                        Just func -> Right st { 
+                            ip = V.length (funcCode func),
+                            stack = [VChar returnValue]
+                        }
+                (Frame{fIP, fFunction}:restFrames) ->
+                    Right st {
+                        ip = fIP,
+                        currentFunc = fFunction,
+                        frames = restFrames,
+                        stack = VChar returnValue : restStack
+                    }
+        _ -> Left "IReturnChar expects a char on top of the stack"
 
 
 controlFlowGoto :: Int -> VMState -> Either String VMState
